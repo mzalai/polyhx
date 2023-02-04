7f961953b6dffac3e1043992b8d58c9e0bcd31c5
Humza <humzaw28@gmail.com>
2023-02-04 18:43:13 -0500

    Added Radar chart and moved around code 4 barChrt

--- drw-challenge/src/App.js
+++ drw-challenge/src/App.js
@@ -1,12 +1,93 @@
-import React from 'react';
-import ReactEcharts from 'echarts-for-react'
 import logo from './logo.svg';
 import './App.css';
 import { Layout, Row, Col, Card } from 'antd';
+import React from "react";
+import ReactEcharts from "echarts-for-react"; 
 
 function App() {
   const firstRowHeight = 400;
   const secondRowHeight = 600;
+  
+
+  const toolsData = [
+    [392,740,575,566,388]
+  ];
+
+  const lineStyle = {
+    width: 1,
+    opacity: 0.5
+  };
+
+  const optionA = {
+    backgroundColor: '#161627',
+    title: {
+      text: 'Tools Used',
+      right: 'left',
+      textStyle: {
+        color: '#eee'
+      }
+    },
+    legend: {
+      bottom: 2,
+      data: ['Tools'],
+      itemGap: 20,
+      textStyle: {
+        color: '#fff',
+        fontSize: 14
+      },
+      selectedMode: 'single'
+    },
+    radar: {
+      indicator: [
+        { name: 'Python', max: 742 },
+        { name: 'R', max: 742 },
+        { name: 'Spark', max: 742 },
+        { name: 'AWS', max: 742 },
+        { name: 'Excel', max: 742 },
+      ],
+      shape: 'circle',
+      splitNumber: 5,
+      axisName: {
+        color: 'rgb(238, 197, 102)'
+      },
+      splitLine: {
+        lineStyle: {
+          color: [
+            'rgba(238, 197, 102, 0.1)',
+            'rgba(238, 197, 102, 0.2)',
+            'rgba(238, 197, 102, 0.4)',
+            'rgba(238, 197, 102, 0.6)',
+            'rgba(238, 197, 102, 0.8)',
+            'rgba(238, 197, 102, 1)'
+          ].reverse()
+        }
+      },
+      splitArea: {
+        show: false
+      },
+      axisLine: {
+        lineStyle: {
+          color: 'rgba(238, 197, 102, 0.5)'
+        }
+      }
+    },
+    series: [
+      {
+        name: 'Tools',
+        type: 'radar',
+        lineStyle: lineStyle,
+        data: toolsData,
+        symbol: 'none',
+        itemStyle: {
+          color: '#F9713C'
+        },
+        areaStyle: {
+          opacity: 0.1
+        }
+      }
+    ]
+  };
+
   const option = {
     xAxis: {
       max: 'dataMax'
@@ -37,13 +118,14 @@ function App() {
     animationEasing: 'linear',
     animationEasingUpdate: 'linear'
   };
+  
   return (
     <div className="App">
       <Layout className="layout">
         <Row gutter={[6, 6]}>
           <Col span={24}>
             <Row gutter={[6, 6]}>
-              <Col span={10}><Card style={{height: firstRowHeight}}>1</Card></Col>
+              <Col span={10}><Card style={{height: firstRowHeight}}> <ReactEcharts option={optionA} />;</Card></Col>
               <Col span={14}><Card style={{height: firstRowHeight}}>1</Card></Col>
             </Row>
           </Col>
