cf193d3ba0a1aff85932e5f119dbe674dd660348
yuri7718 <qiangxu1204@gmail.com>
2023-02-04 15:52:41 -0500

    first commit

---
+++ drw-challenge/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Layout, Row, Col, Card } from 'antd';
+
+function App() {
+  const firstRowHeight = 400;
+  const secondRowHeight = 600;
+  return (
+    <div className="App">
+      <Layout className="layout">
+        <Row gutter={[6, 6]}>
+          <Col span={24}>
+            <Row gutter={[6, 6]}>
+              <Col span={10}><Card style={{height: firstRowHeight}}>1</Card></Col>
+              <Col span={14}><Card style={{height: firstRowHeight}}>1</Card></Col>
+            </Row>
+          </Col>
+          <Col span={24}>
+            <Row gutter={[6, 6]}>
+              <Col span={14}><Card style={{height: secondRowHeight}}>1</Card></Col>
+              <Col span={10}><Card style={{height: secondRowHeight}}>1</Card></Col>
+            </Row>
+          </Col>
+        </Row>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
